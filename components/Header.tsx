83f1c9f8461be2228c475f94be458253375494c5
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-11-28 13:08:39 +0100

    add styling to Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,29 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { BellIcon, SearchIcon } from "@heroicons/react/solid";
+import Link from "next/link";
 interface Props {}
 
 const Header = (props: Props) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img src="https://rb.gy/ulxxee" width={100} height={100} className="cursor-pointer object-contain" />
 
@@ -16,7 +35,15 @@ const Header = (props: Props) => {
           <li className="headerLink">My List</li>
         </ul>
       </div>
-      <div></div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img src="https://rb.gy/g1pwyx" alt="" className="cursor-pointer rounded" />
+        </Link>
+      </div>
     </header>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Netflix+</title>
         <link rel="icon" href="/favicon.ico" />
