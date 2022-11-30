f023b5100b1ec5258c700b679467fa0e0841b75f
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-11-30 17:36:34 +0100

    add custom scrollbar styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ const Home = ({
 
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
+        <section className="md:space-y-24">
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
