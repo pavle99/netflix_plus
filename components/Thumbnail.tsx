6900f1307ec3b1f0338591fa156f1c84c7b312dc
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-12-05 18:58:21 +0100

    add video Modal component

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import useMovieStore from "../store/movieStore";
 import { Movie } from "../typings";
 
 interface IProps {
@@ -6,10 +7,15 @@ interface IProps {
 }
 
 function Thumbnail({ movie }: IProps) {
+  const { showModal, setShowModal, currentMovie, setCurrentMovie } = useMovieStore();
+
   return (
     <div
       className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
-      onClick={() => {}}
+      onClick={() => {
+        setCurrentMovie(movie);
+        setShowModal(true);
+      }}
     >
       <Image
         src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path}`}
