b08481103801966ac0787a9bae65c02075e0a414
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-12-08 12:24:26 +0100

    add stripe function for redirecting to billing portal

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,4 +1,5 @@
 import { createCheckoutSession, getStripePayments } from "@stripe/firestore-stripe-payments";
+import { getFunctions, httpsCallable } from "@firebase/functions";
 import app from "./firebase";
 
 const payments = getStripePayments(app, {
@@ -16,5 +17,16 @@ const loadCheckout = async (priceId: string) => {
     .catch((error) => console.log(error.message));
 };
 
-export { loadCheckout };
+const goToBillingPortal = async () => {
+  const instance = getFunctions(app, "us-central1");
+  const functionRef = httpsCallable(instance, "ext-firestore-stripe-payments-createPortalLink");
+
+  await functionRef({
+    returnUrl: `${window.location.origin}/account`,
+  })
+    .then(({ data }: any) => window.location.assign(data.url))
+    .catch((error) => console.log(error.message));
+};
+
+export { loadCheckout, goToBillingPortal };
 export default payments;
